f9441844d009e54e76ba0c16ab4ebcdd9e5f507d
Manu Sporny <msporny@digitalbazaar.com>
2017-06-23 17:26:49 -0400

    Implement helpers and mock data for HTTP API tests.

--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -6,6 +6,7 @@
 
 var async = require('async');
 var brIdentity = require('bedrock-identity');
+var brKey = require('bedrock-key');
 var database = require('bedrock-mongodb');
 var uuid = require('uuid').v4;
 
@@ -27,6 +28,51 @@ api.createIdentity = function(userName) {
     sysStatus: 'active'
   };
   return newIdentity;
+};
+
+api.createKeyPair = function(options) {
+  const userName = options.userName;
+  const publicKey = options.publicKey;
+  const privateKey = options.privateKey;
+  let ownerId = null;
+  if(userName === 'userUnknown') {
+    ownerId = '';
+  } else {
+    ownerId = options.userId;
+  }
+  const newKeyPair = {
+    publicKey: {
+      '@context': 'https://w3id.org/identity/v1',
+      id: ownerId + '/keys/1',
+      type: 'CryptographicKey',
+      owner: ownerId,
+      label: 'Signing Key 1',
+      publicKeyPem: publicKey
+    },
+    privateKey: {
+      type: 'CryptographicKey',
+      owner: ownerId,
+      label: 'Signing Key 1',
+      publicKey: ownerId + '/keys/1',
+      privateKeyPem: privateKey
+    }
+  };
+  return newKeyPair;
+};
+
+api.createHttpSignatureRequest = function(options) {
+  const newRequest = {
+    url: options.url,
+    httpSignature: {
+      key: options.identity.keys.privateKey.privateKeyPem,
+      keyId: options.identity.keys.publicKey.id,
+      headers: ['date', 'host', 'request-line']
+    }
+  };
+  if(options.body) {
+    newRequest.body = options.body;
+  }
+  return newRequest;
 };
 
 api.removeCollection = function(collection, callback) {
@@ -69,7 +115,18 @@ api.getEventNumber = function(eventId) {
 // Insert identities and public keys used for testing into database
 function insertTestData(mockData, callback) {
   async.forEachOf(mockData.identities, (identity, key, callback) => {
-    brIdentity.insert(null, identity.identity, callback);
+    async.parallel([
+      function(callback) {
+        brIdentity.insert(null, identity.identity, callback);
+      },
+      function(callback) {
+        if(identity.keys) {
+          brKey.addPublicKey(null, identity.keys.publicKey, callback);
+        } else {
+          callback();
+        }
+      }
+    ], callback);
   }, err => {
     if(err) {
       if(!database.isDuplicateError(err)) {
--- test/mocha/mock.data.js
+++ test/mocha/mock.data.js
@@ -19,6 +19,46 @@ identities[userName].identity.sysResourceRole.push({
   sysRole: 'bedrock-ledger-agent.test',
   generateResource: 'id'
 });
+identities[userName].keys = helpers.createKeyPair({
+  userName: userName,
+  userId: identities[userName].identity.id,
+  publicKey: '-----BEGIN PUBLIC KEY-----\n' +
+    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqv8gApfU3FhZx1gyKmBU\n' +
+    'czZ1Ba3DQbqcGRJiwWz6wrr9E/K0PcpRws/+GPc1znG4cKLdxkdyA2zROUt/lbaM\n' +
+    'TU+/kZzRh3ICZZOuo8kJpGqxPDIm7L1lIcBLOWu/UEV2VaWNOENwiQbh61VJlR+k\n' +
+    'HK9LhQxYYZT554MYaXzcSRTC/RzHDTAocf+B1go8tawPEixgs93+HHXoLPGypmqn\n' +
+    'lBKAjmGMwizbWFccDQqv0yZfAFpdVY2MNKlDSUNMnZyUgBZNpGOGPm9zi9aMFT2d\n' +
+    'DrN9fpWMdu0QeZrJrDHzk6TKwtKrBB9xNMuHGYdPxy8Ix0uNmUt0mqt6H5Vhl4O0\n' +
+    '0QIDAQAB\n' +
+    '-----END PUBLIC KEY-----\n',
+  privateKey: '-----BEGIN RSA PRIVATE KEY-----\n' +
+    'MIIEpQIBAAKCAQEAqv8gApfU3FhZx1gyKmBUczZ1Ba3DQbqcGRJiwWz6wrr9E/K0\n' +
+    'PcpRws/+GPc1znG4cKLdxkdyA2zROUt/lbaMTU+/kZzRh3ICZZOuo8kJpGqxPDIm\n' +
+    '7L1lIcBLOWu/UEV2VaWNOENwiQbh61VJlR+kHK9LhQxYYZT554MYaXzcSRTC/RzH\n' +
+    'DTAocf+B1go8tawPEixgs93+HHXoLPGypmqnlBKAjmGMwizbWFccDQqv0yZfAFpd\n' +
+    'VY2MNKlDSUNMnZyUgBZNpGOGPm9zi9aMFT2dDrN9fpWMdu0QeZrJrDHzk6TKwtKr\n' +
+    'BB9xNMuHGYdPxy8Ix0uNmUt0mqt6H5Vhl4O00QIDAQABAoIBAQCpA3yXM42AsY8j\n' +
+    'mwgSnJ48NqJaF5L8P7+UhHi6KMZ+fSYydl0zCevge4bzFD3JrNuZ8VD1b57AxejT\n' +
+    'Ec2so/9vVxjJi1AK6WR3FA608rumGJLQJd4Vd2ojfxabTeWOKOo642R/LSFpPzVE\n' +
+    'T0toqxqiA53IhxhAc2jDLO+PLIvrao0Y8bWWq36tbxsAplrv8Gms6ZRwfKoX5P32\n' +
+    'azBpJOqneNdSMRPHky6t2uiYyuPeG9pbuaClkD7Ss9lpH0V1DLQmAAlP9I0Aa06B\n' +
+    'a9zPFPb3Ae8F0HO/tsf8gIvrlT38JvLe5VuCS7/LQNCZguyPZuZOXLDmdETfm1FD\n' +
+    'q56rCV7VAoGBANmQ7EqDfxmUygTXlqaCQqNzY5pYKItM6RFHc9I+ADBWsLbuKtfP\n' +
+    'XUMHQx6PvwCMBpjZkM7doGdzOHb0l3rW8zQONayqQxN9Pjd7K+dkSY6k0SScw46w\n' +
+    '0AexDQSM/0ahVAHfXXi1GbKwlonM0nn/7JHz7n/fL9HwV8T3hAGClbPDAoGBAMk0\n' +
+    'K5d+Ov55sKW0ZatZ0vTnfBCSrVEfG6FkcyK7uiSsMdWo2/De0VtJF7od2DM5UyP6\n' +
+    'Y/DSVk4oPepbug5oGdu8t1Q3jbS61A7i/dssirQC4hEFAtoTGsVfaH8wu4AKyWd7\n' +
+    '0rUmSrnyqNr4mfQBjdaXByvWO9rdEfZcZqaSQ4/bAoGAKy/CR7Q8eYZ4Z2eoBtta\n' +
+    'gPl5rvyK58PXi8+EJRqbjPzYTSePp5EI8TIy15EvF9uzv4mIXhfOLFrJvYsluoOK\n' +
+    'eS3M575QXEEDJZ40g9T7aO48eakIhH2CfdReQiX+0jVZ6Jk/A6PnOvokl6vpp7/u\n' +
+    'ZLZoBEf4RRMRSQ7czDPwpWMCgYEAlNWZtWuz+hBMgpcqahF9AprF5ICL4qkvSDjF\n' +
+    'Dpltfbk+9/z8DXbVyUANZCi1iFbMUJ3lFfyRySjtfBI0VHnfPvOfbZXWpi1ZtlVl\n' +
+    'UZ7mT3ief9aEIIrnT79ezk9fM71G9NzcphHYTyrYi3pAcAZCRM3diSjlh+XmZqY9\n' +
+    'bNRfU+cCgYEAoBYwp0PJ1QEp3lSmb+gJiTxfNwIrP+VLkWYzPREpSbghDYjE2DfC\n' +
+    'M8pNbVWpnOfT7OlhN3jw8pxHWap6PxNyVT2W/1AHNGKTK/BfFVn3nVGhOgPgH1AO\n' +
+    'sObYxm9gpkNkelXejA/trbLe4hg7RWNYzOztbfbZakdVjMNfXnyw+Q0=\n' +
+    '-----END RSA PRIVATE KEY-----\n'
+});
 
 // identity with no permissions
 userName = 'unauthorizedUser';
