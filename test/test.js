625dcdab3731891ceb099fcc61f78b5ee13718ca
Matthew Collier <collier.matthew@gmail.com>
2017-06-27 20:58:01 -0400

    Update test.js.

--- test/test.js
+++ test/test.js
@@ -2,12 +2,10 @@
  * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
  */
 var bedrock = require('bedrock');
-require('bedrock-identity');
-require('bedrock-ledger');
-require('bedrock-ledger-storage-mongodb');
 require('bedrock-ledger-agent');
-require('bedrock-server');
-require('bedrock-mongodb');
+// require storage plugin
+require('bedrock-ledger-storage-mongodb');
+// require consensus plugin
 require('./consensus');
 
 require('bedrock-test');
