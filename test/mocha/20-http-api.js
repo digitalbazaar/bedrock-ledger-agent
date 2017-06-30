7af51171074d1be2ad6d2ffa116cae52c8fbf840
Manu Sporny <msporny@digitalbazaar.com>
2017-06-29 20:56:16 -0400

    Add tests for event creation and retrieval.

--- test/mocha/20-http-api.js
+++ test/mocha/20-http-api.js
@@ -23,14 +23,42 @@ const urlObj = {
 };
 
 describe('Ledger Agent HTTP API', () => {
+  const regularActor = mockData.identities.regularUser;
+  const configBlock = mockData.blocks.configBlock;
+  let defaultLedgerAgent;
+
+  before(done => helpers.prepareDatabase(mockData, done));
   before(done => {
-    helpers.prepareDatabase(mockData, done);
+    async.auto({
+      add: callback => {
+        const configBlock = mockData.blocks.configBlock;
+        request.post(helpers.createHttpSignatureRequest({
+          url: url.format(urlObj),
+          body: configBlock,
+          identity: regularActor
+        }), (err, res) => {
+          should.not.exist(err);
+          res.statusCode.should.equal(201);
+          callback(null, res.headers.location);
+        });
+      },
+      get: ['add', (results, callback) => {
+        request.get(helpers.createHttpSignatureRequest({
+          url: results.add,
+          identity: regularActor
+        }), (err, res) => {
+          should.not.exist(err);
+          res.statusCode.should.equal(200);
+          defaultLedgerAgent = res.body;
+          callback();
+        });
+      }]
+    }, err => done(err));
   });
   beforeEach(done => {
     helpers.removeCollection('ledger_testLedger', done);
   });
   describe('authenticated as regularUser', () => {
-    const regularActor = mockData.identities.regularUser;
 
     it('should add ledger agent for new ledger', done => {
       const configBlock = mockData.blocks.configBlock;
@@ -65,7 +93,7 @@ describe('Ledger Agent HTTP API', () => {
         }]
       }, err => done(err));
     });
-    it('should get an existing ledger node', done => {
+    it('should get an existing ledger agent', done => {
       async.auto({
         add: callback => {
           const configBlock = mockData.blocks.configBlock;
@@ -85,7 +113,7 @@ describe('Ledger Agent HTTP API', () => {
             identity: regularActor
           }), (err, res) => {
             should.not.exist(err);
-            res.statusCode.should.equal(201);
+            res.statusCode.should.equal(200);
             callback();
           });
         }]
@@ -144,6 +172,88 @@ describe('Ledger Agent HTTP API', () => {
             callback();
           });
         }]
+      }, err => done(err));
+    });
+    it.skip('should add event', done => {
+      async.auto({
+        add: callback => {
+          const event = {
+            '@context': 'https://schema.org/',
+            type: 'Event',
+            name: 'Big Band Concert in New York City',
+            startDate: '2017-07-14T21:30',
+            location: 'https://example.org/the-venue',
+            offers: {
+              type: 'Offer',
+              price: '13.00',
+              priceCurrency: 'USD',
+              url: 'https://www.ticketfly.com/purchase/309433'
+            },
+            signature: {
+              type: 'RsaSignature2017',
+              created: '2017-05-10T19:47:15Z',
+              creator: 'https://www.ticketfly.com/keys/789',
+              signatureValue: 'JoS27wqa...BFMgXIMw=='
+            }
+          };
+
+          request.post(helpers.createHttpSignatureRequest({
+            url: defaultLedgerAgent.service.ledgerEventService,
+            body: event,
+            identity: regularActor
+          }), (err, res) => {
+            should.not.exist(err);
+            res.statusCode.should.equal(201);
+            callback(null, res.headers.location);
+          });
+        }
+      }, err => done(err));
+    });
+    it.skip('should get event', done => {
+      async.auto({
+        add: callback => {
+          const event = {
+            '@context': 'https://schema.org/',
+            type: 'Event',
+            name: 'Big Band Concert in New York City',
+            startDate: '2017-07-14T21:30',
+            location: 'https://example.org/the-venue',
+            offers: {
+              type: 'Offer',
+              price: '13.00',
+              priceCurrency: 'USD',
+              url: 'https://www.ticketfly.com/purchase/309433'
+            },
+            signature: {
+              type: 'RsaSignature2017',
+              created: '2017-05-10T19:47:15Z',
+              creator: 'https://www.ticketfly.com/keys/789',
+              signatureValue: 'JoS27wqa...BFMgXIMw=='
+            }
+          };
+
+          request.post(helpers.createHttpSignatureRequest({
+            url: defaultLedgerAgent.service.ledgerEventService,
+            body: event,
+            identity: regularActor
+          }), (err, res) => {
+            should.not.exist(err);
+            res.statusCode.should.equal(201);
+            callback(null, res.headers.location);
+          });
+        },
+        get: (results, callback) => {
+          const eventUrl = results.add;
+          request.get(helpers.createHttpSignatureRequest({
+            url: eventUrl,
+            identity: regularActor
+          }), (err, res) => {
+            should.not.exist(err);
+            res.statusCode.should.equal(200);
+            res.body.event.startDate.should.equal('2017-07-14T21:30');
+            callback(null, res.body);
+          });
+        }
       }, err => done(err));
     });
   });
