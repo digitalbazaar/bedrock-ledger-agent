0726aa414dfbbeabdcd416a531fa53e3ab6125b4
Manu Sporny <msporny@digitalbazaar.com>
2017-07-02 15:09:39 -0400

    Update to use bedrock-ledger-context.

--- test/test.js
+++ test/test.js
@@ -2,10 +2,10 @@
  * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
  */
 var bedrock = require('bedrock');
+require('bedrock-ledger');
+require('bedrock-ledger-context');
 require('bedrock-ledger-agent');
-// require storage plugin
 require('bedrock-ledger-storage-mongodb');
-// require consensus plugin
 require('bedrock-ledger-consensus-uni');
 
 require('bedrock-test');
