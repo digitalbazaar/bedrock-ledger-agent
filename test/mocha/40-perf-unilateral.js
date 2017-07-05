21ef1ec407b28ea219999c3e5ba64d7c71a0334f
Matthew Collier <collier.matthew@gmail.com>
2017-07-05 17:43:50 -0400

    Update performance tests.

--- test/mocha/40-perf-unilateral.js
+++ test/mocha/40-perf-unilateral.js
@@ -11,6 +11,7 @@ const brLedger = require('bedrock-ledger');
 const brLedgerAgent = require('bedrock-ledger-agent');
 const config = bedrock.config;
 const helpers = require('./helpers');
+const jsigs = require('jsonld-signatures');
 const mockData = require('./mock.data');
 let request = require('request');
 request = request.defaults({json: true, strictSSL: false});
@@ -19,32 +20,53 @@ const url = require('url');
 const uuid = require('uuid/v4');
 const querystring = require('querystring');
 
+jsigs.use('jsonld', bedrock.jsonld);
+
 const urlObj = {
   protocol: 'https',
   host: config.server.host,
   pathname: config['ledger-agent'].routes.agents
 };
 
-describe.skip('Performance - 1 Node - Unilateral - One Signature', () => {
+describe('Performance - 1 Node - Unilateral - One Signature', () => {
   const regularActor = mockData.identities.regularUser;
-  const configEvent = mockData.events.config;
+  const numEvents = 100;
   let ledgerAgent;
-  let currentTest;
+  const signedEvents = [];
 
   before(done => helpers.prepareDatabase(mockData, done));
   before(done => {
     async.auto({
-      add: callback => {
+      generateEvents: callback => async.times(numEvents, (n, callback) => {
+        const concertEvent = _.cloneDeep(mockData.events.concert);
+        concertEvent.input[0].id = 'https://example.com/events/' + uuid();
+        jsigs.sign(concertEvent, {
+          algorithm: 'LinkedDataSignature2015',
+          privateKeyPem:
+            mockData.identities.regularUser.keys.privateKey.privateKeyPem,
+          creator: mockData.identities.regularUser.keys.privateKey.publicKey
+        }, (err, result) => {
+          signedEvents.push(result);
+          callback();
+        });
+      }, callback),
+      signConfig: callback => jsigs.sign(mockData.events.config, {
+        algorithm: 'LinkedDataSignature2015',
+        privateKeyPem:
+          mockData.identities.regularUser.keys.privateKey.privateKeyPem,
+        creator: mockData.identities.regularUser.keys.privateKey.publicKey
+      }, callback),
+      add: ['signConfig', (results, callback) => {
         request.post(helpers.createHttpSignatureRequest({
           url: url.format(urlObj),
-          body: mockData.events.config,
+          body: results.signConfig,
           identity: regularActor
         }), (err, res) => {
           should.not.exist(err);
           res.statusCode.should.equal(201);
           callback(null, res.headers.location);
         });
-      },
+      }],
       get: ['add', (results, callback) => {
         request.get(helpers.createHttpSignatureRequest({
           url: results.add,
@@ -61,16 +83,13 @@ describe.skip('Performance - 1 Node - Unilateral - One Signature', () => {
   beforeEach(done => {
     helpers.removeCollection('ledger_testLedger', done);
   });
-  const events = 100;
   let eventsPerSecond = 0;
-  it('adds ' + events + ' events and blocks', done => {
+  it('adds ' + numEvents + ' events and blocks', done => {
     const start = Date.now();
-    async.times(events, (n, callback) => {
-      const concertEvent = _.cloneDeep(mockData.events.concert);
-      concertEvent.input[0].id = 'https://example.com/events/' + uuid(),
+    async.times(numEvents, (n, callback) => {
       request.post(helpers.createHttpSignatureRequest({
         url: ledgerAgent.service.ledgerEventService,
-        body: concertEvent,
+        body: signedEvents[n],
         identity: regularActor
       }), (err, res) => {
         should.not.exist(err);
@@ -80,13 +99,13 @@ describe.skip('Performance - 1 Node - Unilateral - One Signature', () => {
     }, err => {
       const end = Date.now();
       const totalTime = (end - start) / 1000;
-      eventsPerSecond = events / totalTime;
+      eventsPerSecond = numEvents / totalTime;
       done(err);
-    })
+    });
   }).timeout(30000);
   it('should perform more than 5 transactions per second', done => {
-    console.log('        ' + Math.floor(eventsPerSecond) + ' events per second');
+    console.log('       ' + Math.floor(eventsPerSecond) + ' events per second');
     eventsPerSecond.should.be.at.least(5);
     done();
-  })
+  });
 });
